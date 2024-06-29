c7a64899a2f59e1e2afb144949addc3575bd9e05
CM-Silence <757902252@qq.com>
2024-06-29 16:29:32 +0800

    [conf]配置改动

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,13 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   lintOnSave: false,
-  transpileDependencies: true,
-  devServer: {
-    proxy: {
-      '/user': {
-        target: 'http://localhost:8217', // 这里只是示例，通常你应该指向你的后端API服务器
-        changeOrigin: true, // 允许跨域
-      }
-    }
-  }
 })
